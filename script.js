2e53726c3a41c1a82520be24c57caac8fa2493ec
Muhammad Hanif Nahara <47244906+muhamadhanifnahara7@users.noreply.github.com>
2026-02-04 22:09:38 +0700

    Remove animejs animation for h2 elements
    Removed animation functionality using animejs for h2 elements.

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
 // Wait for the DOM to be fully loaded before running any scripts
-import { animate, stagger, splitText } from 'animejs';
-
-const { chars } = splitText('h2', { words: false, chars: true });
-
-animate(chars, {
-  // Property keyframes
-  y: [
-    { to: '-2.75rem', ease: 'outExpo', duration: 600 },
-    { to: 0, ease: 'outBounce', duration: 800, delay: 100 }
-  ],
-  // Property specific parameters
-  rotate: {
-    from: '-1turn',
-    delay: 0
-  },
-  delay: stagger(50),
-  ease: 'inOutCirc',
-  loopDelay: 1000,
-  loop: true
-});
         document.addEventListener('DOMContentLoaded', function() {
         
             // --- Mobile Menu Functionality ---
